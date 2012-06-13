d688d4f46e83545aba5183bdcf9da1d7572f81ab
Jason Madsen <knomedia@gmail.com>
2012-06-13 17:35:48 -0400

    Grabs hash and pushes to editor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready( function () {
 	$('#editor').redactor({ fullscreen: true });
+	var data = window.location.hash.replace('#', '');
+	window.setTimeout( setHTML, 300, data);
 });
 
 function setHTML( value ) {
